c392a98120a7ec42b3536a9d256d627d54b4ffcd
bdjm <brendandjmoore@gmail.com>
2021-07-19 21:30:36 +1000

    adding function that handles a submit message

--- src/components/views/contact/contact.js
+++ src/components/views/contact/contact.js
@@ -2,3 +2,23 @@ import React, {useState } from 'react';
 import emailjs from 'emailjs-com';
 import './style.css'
 
+export default function Contact(props) {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    let handleSubmit = event => {
+        event.preventDefault()
+        let mailKey = process.env.REACT_APP_EMAILJS_KEY
+        emailjs.sendForm('portfolio', 'contact-form', event.target, mailKey)
+        .then((result) => {
+            console.log(result.text)
+            setName('')
+            setEmail('')
+            setMessage('')
+            window.alert('Your message has been received, I will get back to you soon! Thanks!')
+        }, (error) => {
+            console.log(`Error! ${error.text}`)
+        })
+    }
+}
