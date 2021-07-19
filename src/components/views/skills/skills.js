c02de81c43681066c134d086be37bb5b9aa658cf
bdjm <brendandjmoore@gmail.com>
2021-07-19 18:50:11 +1000

    adding the content for the skills page

--- src/components/views/skills/skills.js
+++ src/components/views/skills/skills.js
@@ -3,3 +3,24 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import ReactWordCloud from 'react-wordcloud';
 import './style.css';
 import languages from './languages';
+
+export default function Skills(props) {
+    const options = {
+        colors: ['#ffffff', '#535353', '#8f8f8f'],
+        enableOptimizations: true,
+        fontFamily: 'impact',
+        fontSizes: [20, 60],
+        rotations: 1,
+        rotationAngles: [0, 90],
+    };
+
+    return (
+        <div className='content' id='skills'>
+            <p>See below for the skills and languages that I have learnt!</p>
+            <ReactWordCloud
+            words={languages}
+            options={options}
+            />
+        </div>
+    )
+}
