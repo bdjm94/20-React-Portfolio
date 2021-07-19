3f4296f061a3b1af4dba1bc0126e25ac5f54bd64
bdjm <brendandjmoore@gmail.com>
2021-07-19 19:20:15 +1000

    adding linkedin, github and email icons + links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,3 +3,14 @@ import email from '../../images/email.png';
 import github from '../../images/github.png';
 import linkedin from '../../images/linkedin.png';
 import './style.css';
+
+export default function Footer(props) {
+    const arr =[
+        <div key='0' className='w3-animate-left'><a href='https://www.linkedin.com/in/bdjm94/' target='_blank' rel='noopener noreferrer'>
+            <img className='ind-icon' alt='LinkedIn' src={linkedin} /></a></div>,
+        <div key='1' className='w3-animate-left'><a href='https://github.com/bdjm94/' target='_blank' rel='noopener noreferrer'>
+            <img className='ind-icon' alt='GitHub' src={github} /></a></div>,
+        <div key='0' className='w3-animate-left'><a href='mailto:brendandjmoore@gmail.com' target='_blank' rel='noopener noreferrer'>
+            <img className='ind-icon' alt='Email' src={email} /></a></div>
+    ]
+}
