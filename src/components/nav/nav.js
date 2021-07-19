ee66f54703678a7019eba69890e8d920519347cb
bdjm <brendandjmoore@gmail.com>
2021-07-19 13:18:27 +1000

    adding the nav bar script

---
+++ src/components/nav/nav.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect} from 'react';
+import './style.css';
+
+function Nav (props) {
+    const [elements, setElements] = useState([])
+
+    useEffect(() => {
+        let count = 0
+        let timer = setInterval(() => {
+            setElements(elements => [...elements, props.children[count]])
+            count++
+            if (count === props.children.length) {
+                clearInterval(timer)
+            }
+        }, 100)
+    }, [props.children])
+
+    return (
+        <div id='nav-wrap'>
+            <nav className='nav'>
+                {elements}
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
