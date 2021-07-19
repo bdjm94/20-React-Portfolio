8dd3671b5deed8662df89180faceeb75933ae471
bdjm <brendandjmoore@gmail.com>
2021-07-19 15:45:29 +1000

    adding the rest of the routes to the app.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Nav from './components/nav/nav';
 import NavComp from './components/navcomp/navcomp';
 import { BrowserRouter, HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import About from './components/views/about/about';
-
+import Portfolio from './components/views/portfolio/portfolio';
+import Skills from './components/views/skills/skills';
+import Contact from './components/views/contact/contact';
+import Footer from './components/footer/footer';
+import Content from './components/content/content';
 import './App.css';
 
 function App() {
