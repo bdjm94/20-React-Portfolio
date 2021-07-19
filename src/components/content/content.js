c286e66cfa09a205e702847d2e2dac98816858c7
bdjm <brendandjmoore@gmail.com>
2021-07-19 14:18:15 +1000

    fixing up the import react code

---
+++ src/components/content/content.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import './style.css';
+
+export default function Content (props) {
+    const [content, setContent] = useState([])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setContent(props.children)
+        }, 500)
+    }, [])
+
+    return (
+        content
+    )
+}
