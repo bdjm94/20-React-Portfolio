f6aeb8adea5b8262a206e2bf86ff16ac00a928ca
bdjm <brendandjmoore@gmail.com>
2021-07-19 18:46:27 +1000

    adding the imports for the skills page

---
+++ src/components/views/skills/skills.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import ReactWordCloud from 'react-wordcloud';
+import './style.css';
+import languages from './languages';
