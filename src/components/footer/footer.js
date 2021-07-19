c5bce71669ebcbf64985f41de35180ecfc439b24
bdjm <brendandjmoore@gmail.com>
2021-07-19 19:15:23 +1000

    adding the relevant imports for the footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import email from '../../images/email.png';
+import github from '../../images/github.png';
+import linkedin from '../../images/linkedin.png';
+import './style.css';
